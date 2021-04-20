e2f8f74331e09b619f716c9790a293e900a577f2
Wordyka <wordykanainggolan1212@gmail.com>
2021-04-20 23:25:31 +0700

    Commit 1 Daftar Buku

---
+++ app/Http/Controllers/DataBukuController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DataBuku;
+
+class DataBukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = DataBuku::all();
+        return view('DataBuku.index', ['data'=>$data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('DataBuku.tambah');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        DataBuku::create([
+            'id_buku' => $request->id_buku,
+            'judul' => $request->judul,
+            'penerbit' => $request->penerbit,
+            'rak' => $request->rak,
+            'gambar' => $request->gambar
+        ]);
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $DataBuku = DataBuku::find($id);
+        return view('DataBuku.ubah', ['DataBuku' => $DataBuku]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/', 'DataBukuController@index');
+
+Route::get('/tambah', 'DataBukuController@create');
+
+Route::post('/simpan', 'DataBukuController@store');
+
+Route::post('/ubah', 'DataBukuController@show');
+
+
