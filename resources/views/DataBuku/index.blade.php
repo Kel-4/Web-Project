c88e5180f59523f0cd1146479c3b44a2d073d580
Wordyka <wordykanainggolan1212@gmail.com>
2021-04-21 21:02:50 +0700

    Commit 3 Daftar Buku UI design

--- resources/views/DataBuku/index.blade.php
+++ resources/views/DataBuku/index.blade.php
@@ -1,164 +1,89 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
-    <!-- Iconify Script -->
-    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+        <!-- Icon -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-    <title>Halaman Daftar Buku</title>
-    <style>
-        
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-            text-decoration: none;
-            font-family: 'Quicksand', sans-serif;
-        }
+        <link rel="stylesheet" href="css/style.css">
 
-        body{
-            background-color: #f3f5f9;
-        }
-
-        .wrapper{
-            display: flex;
-            position: relative;
-        }
-
-        .wrapper .sidebar{
-            width: 220px;
-            height: 100%;
-            background: #4b4276;
-            padding: 30px 0px;
-            position: fixed;
-        }
-
-        .wrapper .sidebar h2{
-            color: #fff;
-            text-transform: uppercase;
-            text-align: center;
-            margin-bottom: 30px;
-        }
-
-        .wrapper .sidebar ul li{
-            padding-top: 30px;
-            padding-bottom: 30px;
-            padding-left: 10px;
-            border-bottom: 1px solid #bdb8d7;
-            border-bottom: 1px solid rgba(0,0,0,0.05);
-            border-top: 1px solid rgba(255,255,255,0.05);
-        }    
-
-        .wrapper .sidebar ul li a{
-            color: #bdb8d7;
-            display: block;
-        }
-
-        .wrapper .sidebar ul li a .fas{
-            width: 100px;
-        }
-
-        .wrapper .sidebar ul li:hover{
-            background-color: #594f8d;
-        }
-            
-        .wrapper .sidebar ul li:hover a{
-            color: #fff;
-        }
-        
-
-        .wrapper .main_content{
-            width: 100%;
-            margin-left: 220px;
-        }
-
-        .wrapper .main_content .header{
-            padding: 20px;
-            background: #fff;
-            color: #717171;
-            border-bottom: 1px solid #e0e4e8;
-        }
-
-        .wrapper .main_content .info{
-            margin: 20px;
-            color: #717171;
-            line-height: 25px;
-        }
-
-        .wrapper .main_content .info div{
-            margin-bottom: 20px;
-        }
-
-        a {
-            text-decoration: none;
-        }
-    </style>
-  </head>
-  <body>
-
-    <div class="wrapper">
-        <div class="sidebar">
-            <h2>Delibrary</h2>
-            <ul>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="entypo:book" style="font-size: 24px; color: white;"></span>&nbsp;Daftar Buku</a></li>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="clarity:group-solid" style="font-size: 24px; color: white;"></span>&nbsp;Data Pengunjung</a></li>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="ic:sharp-library-add" style="font-size: 24px; color: white;"></span>&nbsp;Peminjaman</a></li>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="ic:baseline-library-add-check" style="font-size: 24px; color: white;"></span>&nbsp;Pengembalian</a></li>
-                
-            </ul> 
-        </div>
-        <div class="main_content">
-            <div class="header">Welcome!! Have a nice day.</div>  
-            <div class="container">
-                <br>
-                <center><h1>DAFTAR BUKU</h1></center><br><br>
-                <a href="/tambah" class="badge bg-success">Tambah Buku</a><br><br>
-                <table class="table">
-                    <thead class="table-dark">
-                        <tr>
-                            <th>ID</th>
-                            <th>Judul</th>
-                            <th>Penerbit</th>
-                            <th>Rak</th>
-                            <th>Gambar</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($data as $buku)
-                        <tr>
-                            <td>{{ $buku->id_buku }}</td>
-                            <td>{{ $buku->judul }}</td>
-                            <td>{{ $buku->penerbit }}</td>
-                            <td>{{ $buku->rak }}</td>
-                            <td>{{ $buku->gambar }}</td>
-                            <td>
-                                <a href="/hapus/{{ $buku->id }}" class="badge bg-danger">hapus</a>
-                                <a href="/ubah/{{ $buku->id }}" class="badge bg-warning">ubah</a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+        <title>Halaman Daftar Buku</title>
+    </head>
+    <body>
+  
+    <nav class="navbar">
+        <div class="container">
+            <label for="menu-bar" style="cursor: pointer;">
+            <i class="fas fa-bars fa-2x"></i>
+            </label>
+            <h1 class="fw-bold" style="color: blue;"><img src="images/logo.png" style="width: 70px;" alt=""> del<span style="color: red;">ibr</span><span style="color: #41A0FF;">ary</h1>
+            <div class="navbar2">
+            <div class="d-flex">
+                <h5><i class="fas fa-user-circle"></i> User</h5>
+            </div>
             </div>
         </div>
+    </nav>
+
+    <input type="checkbox" id="menu-bar" style="display: none;">
+    <div class="side-navbar container">
+        <ul>
+            <br>
+            <li><a href=""><i class="fas fa-book"></i> &nbsp;Daftar Buku</a></li>
+            <li><a href=""><i class="fas fa-users"></i> &nbsp;Data Pengunjung</a> </li>
+            <li><a href=""><i class="fas fa-address-book"></i> &nbsp;Peminjaman</a> </li>
+            <li><a href=""><i class="fas fa-calendar-check"></i> &nbsp;Pengembalian</a> </li>
+        </ul>
+    </div> 
+
+    <div class="container">
+        <br>
+        <center><h1>DAFTAR BUKU</h1></center><br><br>
+        <a href="/tambah" class="badge bg-success">Tambah Buku</a><br><br>
+        <table class="table rounded-3 table-bordered table-secondary ">
+            <thead class="table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Judul</th>
+                    <th>Penerbit</th>
+                    <th>Rak</th>
+                    <th>Gambar</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data as $buku)
+                <tr>
+                    <td>{{ $buku->id_buku }}</td>
+                    <td>{{ $buku->judul }}</td>
+                    <td>{{ $buku->penerbit }}</td>
+                    <td>{{ $buku->rak }}</td>
+                    <td>{{ $buku->gambar }}</td>
+                    <td>
+                        <a href="/hapus/{{ $buku->id }}" class="badge bg-danger">hapus</a>
+                        <a href="/ubah/{{ $buku->id }}" class="badge bg-warning">ubah</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-    
+
+    <footer class="bg-dark fixed-bottom">
+    <div class="p-2">
+        <h6 class="text-center text-light">delibrary Copyright&copy; Aplikasi Pengelolaan Perpustakaan. By <b class="text-warning">Kelompok 4</b> with <i class="fas fa-heart text-danger"></i></h6>
+    </div>
+    </footer>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
--- resources/views/DataBuku/tambah.blade.php
+++ resources/views/DataBuku/tambah.blade.php
@@ -1,166 +1,91 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
-    <!-- Iconify Script -->
-    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+        <!-- Iconify Script -->
+        <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
 
-    <title>Halaman Daftar Buku</title>
-    <style>
-        
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-            text-decoration: none;
-            font-family: 'Quicksand', sans-serif;
-        }
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-        body{
-            background-color: #f3f5f9;
-        }
-
-        .wrapper{
-            display: flex;
-            position: relative;
-        }
-
-        .wrapper .sidebar{
-            width: 220px;
-            height: 100%;
-            background: #4b4276;
-            padding: 30px 0px;
-            position: fixed;
-        }
-
-        .wrapper .sidebar h2{
-            color: #fff;
-            text-transform: uppercase;
-            text-align: center;
-            margin-bottom: 30px;
-        }
-
-        .wrapper .sidebar ul li{
-            padding-top: 30px;
-            padding-bottom: 30px;
-            padding-left: 10px;
-            border-bottom: 1px solid #bdb8d7;
-            border-bottom: 1px solid rgba(0,0,0,0.05);
-            border-top: 1px solid rgba(255,255,255,0.05);
-        }    
-
-        .wrapper .sidebar ul li a{
-            color: #bdb8d7;
-            display: block;
-        }
-
-        .wrapper .sidebar ul li a .fas{
-            width: 100px;
-        }
-
-        .wrapper .sidebar ul li:hover{
-            background-color: #594f8d;
-        }
-            
-        .wrapper .sidebar ul li:hover a{
-            color: #fff;
-        }
-        
-
-        .wrapper .main_content{
-            width: 100%;
-            margin-left: 220px;
-        }
-
-        .wrapper .main_content .header{
-            padding: 20px;
-            background: #fff;
-            color: #717171;
-            border-bottom: 1px solid #e0e4e8;
-        }
-
-        .wrapper .main_content .info{
-            margin: 20px;
-            color: #717171;
-            line-height: 25px;
-        }
-
-        .wrapper .main_content .info div{
-            margin-bottom: 20px;
-        }
-
-        a {
-            text-decoration: none;
-        }
-    </style>
-  </head>
+        <link rel="stylesheet" href="css/style.css">
+        <title>Halaman Daftar Buku</title>
+    </head>
   <body>
-
-    <div class="wrapper">
-        <div class="sidebar">
-            <h2>Delibrary</h2>
-            <ul>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="entypo:book" style="font-size: 24px; color: white;"></span>&nbsp;Daftar Buku</a></li>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="clarity:group-solid" style="font-size: 24px; color: white;"></span>&nbsp;Data Pengunjung</a></li>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="ic:sharp-library-add" style="font-size: 24px; color: white;"></span>&nbsp;Peminjaman</a></li>
-
-                <li><a href="#"><span class="iconify" data-inline="false" data-icon="ic:baseline-library-add-check" style="font-size: 24px; color: white;"></span>&nbsp;Pengembalian</a></li>
-                
-            </ul> 
-        </div>
-        <div class="main_content">
-            <div class="header">Welcome!! Have a nice day.</div>  
-            <div class="container">
-                <br>
-                <center><h1>TAMBAH DATA</h1></center><br><br>
-                <a href="/" class="badge bg-info">Kembali</a><br><br>
-                
-                <form action="/simpan" method="post">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <label>ID</label>
-                        <input type="text" name="id_buku" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Judul</label>
-                        <input type="text" name="judul" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Penerbit</label>
-                        <input type="text" name="penerbit" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Rak</label>
-                        <input type="text" name="rak" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label>Gambar</label>
-                        <input type="file" name="gambar" class="form-control">
-                    </div>
-                    <div class="form-group">
-                    <br><br>
-                        <button type="submit" class="btn btn-success">Tambah</button>
-                    </div>
-                </form>
-
+    
+    <nav class="navbar">
+        <div class="container">
+            <label for="menu-bar" style="cursor: pointer;">
+            <i class="fas fa-bars fa-2x"></i>
+            </label>
+            <h1 class="fw-bold" style="color: blue;"><img src="images/logo.png" style="width: 70px;" alt=""> del<span style="color: red;">ibr</span><span style="color: #41A0FF;">ary</h1>
+            <div class="navbar2">
+            <div class="d-flex">
+                <h5><i class="fas fa-user-circle"></i> User</h5>
+            </div>
             </div>
         </div>
+    </nav>
+
+    <input type="checkbox" id="menu-bar" style="display: none;">
+    <div class="side-navbar container">
+        <ul>
+            <br>
+            <li><a href=""><i class="fas fa-book"></i> &nbsp;Daftar Buku</a></li>
+            <li><a href=""><i class="fas fa-users"></i> &nbsp;Data Pengunjung</a> </li>
+            <li><a href=""><i class="fas fa-address-book"></i> &nbsp;Peminjaman</a> </li>
+            <li><a href=""><i class="fas fa-calendar-check"></i> &nbsp;Pengembalian</a> </li>
+        </ul>
+    </div> 
+
+    <div class="container">
+        <br>
+        <center><h1>TAMBAH DATA</h1></center><br><br>
+        <a href="/" class="badge bg-info">Kembali</a><br><br>
+        
+        <form action="/simpan" method="post">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label>ID</label>
+                <input type="text" name="id_buku" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Judul</label>
+                <input type="text" name="judul" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Penerbit</label>
+                <input type="text" name="penerbit" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Rak</label>
+                <input type="text" name="rak" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Gambar</label>
+                <input type="file" name="gambar" class="form-control">
+            </div>
+            <div class="form-group">
+            <br><br>
+                <button type="submit" class="btn btn-success">Tambah</button>
+            </div>
+        </form>
     </div>
-    
+
+    <footer class="bg-dark fixed-bottom">
+        <div class="p-2">
+            <h6 class="text-center text-light">delibrary Copyright&copy; Aplikasi Pengelolaan Perpustakaan. By <b class="text-warning">Kelompok 4</b> with <i class="fas fa-heart text-danger"></i></h6>
+        </div>
+    </footer>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
