a3a079f6381f3fef278994da685e75da0e8a7cb8
Sopianna Siagian <82864489+Sopianna082@users.noreply.github.com>
2021-04-22 15:35:00 +0700

    DataPengunjung

---
+++ app/Models/Pengunjung.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pengunjung extends Model
+{
+    use HasFactory;
+    protected $table = 'data_pengunjung';
+    protected $fillable =['id_pengunjung', 'nama', 'tanggal_terdaftar', 'kontak', 'alamat'];
+    protected $primaryKey = 'id';
+}
