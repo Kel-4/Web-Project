55dba92a5d71120d08e39f91db46c372d9281086
Rizki24-star <kikingar.567@gmail.com>
2021-04-24 13:03:45 +0700

    adding sweetalert

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="{{ asset('css/loginstyle.css') }}" >
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="icon" href="{{ asset('img/logo.png') }}">
@@ -26,16 +27,20 @@
         <label for="password"><i class="fas fa-key"></i> Password</label>
         <input id="password" class="form-control" type="password" name="password"><br><br>
         <center><button class="btn btn">Login</button></center><br>
-        @if (session('message'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        
+        {{-- <div class="alert alert-danger alert-dismissible fade show" role="alert">
              <strong>{{ session('message') }}</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>
-        @endif
+          </div> --}}
     </form>
     </div>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    @if (session('message'))
+    <script src="{{ asset('js/loginalert.js') }}"></script>
+    @endif
     <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    @include('sweetalert::alert')
   </body>
   <script type="" src="{{ asset('js/script.js') }}"></script>
 </html>
